ede738ac73637d10c4e19bf861b22fd29b7a8f95
AlecoPB <ajoaquinpg@gmail.com>
2024-10-10 15:50:17 +0200

    Update 0-lockboxes.py

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -2,17 +2,19 @@
 
 
 def canUnlockAll(boxes):
-
     found_keys = set(boxes[0])
-    open = [0]
+    open_boxes = {0}
 
-    while len(open) <= len(boxes):
-        n = found_keys[0]
-        if n in found_keys and n not in open:
-            found_keys.append(set(boxes[n]))
-            open.append(n)
-        elif n in open:
-            idx = 0
-            while n in open:
-                idx += 1
-                n = found_keys[idx]
+    while True:
+        new_keys = set()
+        for key in found_keys:
+            if key < len(boxes) and key not in open_boxes:
+                new_keys.update(boxes[key])
+                open_boxes.add(key)
+                
+        if not new_keys:
+            break
+
+        found_keys.update(new_keys)
+
+    return len(open_boxes) == len(boxes)
