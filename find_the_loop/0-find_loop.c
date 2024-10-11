c69eee10b5811603679e88fc3e657d3fb604dbde
AlecoPB <ajoaquinpg@gmail.com>
2024-10-11 11:33:42 +0200

    Update 0-find_loop.c

--- find_the_loop/0-find_loop.c
+++ find_the_loop/0-find_loop.c
@@ -2,25 +2,28 @@
 
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *slow = head, *fast = head;
+listint_t *slow = head, *fast = head;
 
-    while (fast != NULL && fast->next != NULL) {
-        slow = slow->next;
-        fast = fast->next->next;
+while (fast != NULL && fast->next != NULL)
+{
+slow = slow->next;
+fast = fast->next->next;
 
-        // If the slow pointer meets the fast pointer, there is a loop
-        if (slow == fast) {
-            slow = head;
+// If the slow pointer meets the fast pointer, there is a loop
+if (slow == fast)
+{
+slow = head;
 
-            // Move both pointers one step at a time
-            while (slow != fast) {
-                slow = slow->next;
-                fast = fast->next;
-            }
-            // Return the node where the loop starts
-            return slow;
-        }
-    }
-    // No loop found
-    return NULL;
+// Move both pointers one step at a time
+while (slow != fast)
+{
+slow = slow->next;
+fast = fast->next;
+}
+// Return the node where the loop starts
+return slow;
+}
+}
+// No loop found
+return NULL;
 }
