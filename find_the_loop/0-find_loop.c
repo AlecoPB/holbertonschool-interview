22bc4c7df801a182451ab71d268736adad839fc1
AlecoPB <ajoaquinpg@gmail.com>
2024-10-11 11:30:27 +0200

    a

---
+++ find_the_loop/0-find_loop.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+listint_t *find_listint_loop(listint_t *head)
+{
+    listint_t *slow = head, *fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        // If the slow pointer meets the fast pointer, there is a loop
+        if (slow == fast) {
+            slow = head;
+
+            // Move both pointers one step at a time
+            while (slow != fast) {
+                slow = slow->next;
+                fast = fast->next;
+            }
+            // Return the node where the loop starts
+            return slow;
+        }
+    }
+    // No loop found
+    return NULL;
+}
---
+++ find_the_loop/README.md
@@ -0,0 +1,1 @@
+Function that finds the loop in a linked list
