0715d95f2b830299937bd67c018d271b85156024
AlecoPB <ajoaquinpg@gmail.com>
2024-10-11 11:47:26 +0200

    Update 0-find_loop.c

--- find_the_loop/0-find_loop.c
+++ find_the_loop/0-find_loop.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
+listint_t *find_listint_loop(listint_t *head)
 /**
  * find_listing_loop - find loop
  * @head: points to head
  * Description: Find a loop on a single linked list
  * Return: node of looping
  */
-listint_t *find_listint_loop(listint_t *head)
 {
 listint_t *slow = head, *fast = head;
 
