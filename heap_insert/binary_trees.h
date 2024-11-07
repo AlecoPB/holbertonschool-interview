306999b6bdad5a6ba48f16688dd5dcae19bab133
AlecoPB <ajoaquinpg@gmail.com>
2024-11-07 11:27:08 +0100

    Update binary_trees.h

--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -1,28 +1,20 @@
-/**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
 #include <stddef.h>
 #include <stdlib.h>
 
-struct binary_tree_s
+/* Basic binary tree structure */
+typedef struct binary_tree_s
 {
     int n;
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-};
+} binary_tree_t, heap_t;
 
-typedef struct binary_tree_s binary_tree_t;
-
-typedef struct binary_tree_s heap_t;
-
+/* Function prototypes */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 heap_t *heap_insert(heap_t **root, int value);
 
-// Function prototype for binary_tree_node
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+#endif /* BINARY_TREES_H */
