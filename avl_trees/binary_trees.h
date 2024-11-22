8de8f97f6feb92980a7c698b1646ebbff65940b3
AlecoPB <ajoaquinpg@gmail.com>
2024-11-22 10:12:26 +0100

    Update binary_trees.h

--- avl_trees/binary_trees.h
+++ avl_trees/binary_trees.h
@@ -3,22 +3,18 @@
 
 #include <stddef.h>
 
-/**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
-struct binary_tree_s
+/* Basic Binary Tree Structure */
+typedef struct binary_tree_s
 {
     int n;
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-};
+} binary_tree_t;
 
-typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s avl_t;
+
+/* Function Prototype */
+int binary_tree_is_avl(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
