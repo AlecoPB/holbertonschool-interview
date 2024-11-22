d5a82c4e162bffb91d484eb3c15053382f875378
AlecoPB <ajoaquinpg@gmail.com>
2024-11-22 11:55:53 +0100

    a

---
+++ minimum_operations/0-minioperations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+finds minimum operations
+"""
+import sympy as sp
+
+
+def find_prime_factors(n):
+    if n % 2 = 0:
+        mult = 2
+    elif 
+
+def minOperations(n):
+    initial_mult = 0
+    op = 0
+    # if uneven H => c => p = HH => p = HHH  
+    return op
