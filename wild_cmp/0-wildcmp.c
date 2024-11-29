6b588282f1d30ddd1efbf92c2d279296950176c4
AlecoPB <ajoaquinpg@gmail.com>
2024-11-29 17:42:56 +0100

    Update 0-wildcmp.c

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * wildcmp - compare two strings
+ * @s1: first string
+ * @s2: second string
+ * Description: Compare two strings
+ * Return: 1 or 0
+ */
 int wildcmp(char *s1, char *s2) {
 if (*s1 == '\0' && *s2 == '\0') {
 // Both strings are fully traversed
