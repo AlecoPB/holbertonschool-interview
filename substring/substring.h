aa89d5edeeb6d5b964ac895ee82d3eb7846e267f
AlecoPB <ajoaquinpg@gmail.com>
2024-12-19 16:28:53 +0100

    Update substring.h

--- substring/substring.h
+++ substring/substring.h
@@ -1,8 +1,8 @@
-#ifndef SUBSTRING.H
-#define SUBSTRING.H
+#ifndef SUBSTRING_H
+#define SUBSTRING_H
 
 #include <stddef.h>
 
 int *find_substring(char const *s, char const **words, int nb_words, int *n);
 
-#endif /* SUBSTRING.H */
+#endif /* SUBSTRING_H */
