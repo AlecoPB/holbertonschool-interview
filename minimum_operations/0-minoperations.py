08f633eee66aeff45f21e380df689d830d45b66c
AlecoPB <ajoaquinpg@gmail.com>
2024-12-19 17:02:18 +0100

    Update 0-minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -7,14 +7,14 @@ finds minimum operations
 def minOperations(n):
     if n < 2:
         return 0  # Cannot achieve n < 2
-    
+
     operations = 0
     factor = 2
-    
+
     while n > 1:
         while n % factor == 0:
             operations += factor  # Add operations: factor pastes + 1 copy all
             n //= factor
         factor += 1
-    
+
     return operations
