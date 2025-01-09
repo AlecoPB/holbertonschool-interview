390934efc16d10d480cfc8cee64d0791871a87a6
AlecoPB <ajoaquinpg@gmail.com>
2025-01-09 11:18:19 +0100

    Updated header and script

---
+++ menger/menger.h
@@ -0,0 +1,13 @@
+#ifndef MENGER_H
+#define MENGER_H
+
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * menger - Draws a 2D Menger Sponge of a given level
+ * @level: The level of the Menger Sponge to draw
+ */
+void menger(int level);
+
+#endif /* MENGER_H */
