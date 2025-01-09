94fa1c1fe155dfd24678cac3fe3d5213850630c6
AlecoPB <ajoaquinpg@gmail.com>
2025-01-09 16:48:39 +0100

    Update 0-sorted_array_to_avl.c

--- sorted_array_to_avl/0-sorted_array_to_avl.c
+++ sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -30,7 +30,8 @@ return (new_node);
  *
  * Return: Pointer to the root node of the AVL tree
  */
-avl_t *sorted_array_to_avl_helper(int *array, int start, int end, avl_t *parent)
+avl_t *sorted_array_to_avl_helper(int *array, int start,
+int end, avl_t *parent)
 {
 if (start > end)
 {
@@ -54,7 +55,7 @@ return (node);
  * sorted_array_to_avl - Converts a sorted array to an AVL tree
  * @array: Pointer to the first element of the array
  * @size: Number of elements in the array
- * 
+ *
  * Return: Pointer to the root node of the created AVL tree, or NULL on failure
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
